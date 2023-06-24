f5abefbe9a30829619c97965f0fdc1f7f258405e
Mailsnag <admin@mailsnag.com>
2023-06-24 13:29:06 -0500

    * add turbo drive

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,16 +1,23 @@
+import * as Turbo from "@hotwired/turbo";
+Turbo.start();
+
 import { createApp, defineAsyncComponent } from "vue";
+import TurbolinksAdapter from "vue-turbolinks";
 import vuetify from "~/plugins/vuetify";
 
 import App from "~/app";
 const BlogsIndex = defineAsyncComponent(() => import("~/pages/blogs/index"));
 const BlogsShow = defineAsyncComponent(() => import("~/pages/blogs/show"));
 
-const app = createApp({});
+document.addEventListener("turbo:load", () => {
+  const app = createApp({});
 
-app.use(vuetify);
+  app.use(TurbolinksAdapter);
+  app.use(vuetify);
 
-app.component("App", App);
-app.component("BlogsIndex", BlogsIndex);
-app.component("BlogsShow", BlogsShow);
+  app.component("App", App);
+  app.component("BlogsIndex", BlogsIndex);
+  app.component("BlogsShow", BlogsShow);
 
-app.mount("#app");
+  app.mount("#app");
+});
