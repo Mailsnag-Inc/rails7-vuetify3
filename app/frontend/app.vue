3fb938cabde1b12472123b0065bd5f47506f6564
Mailsnag <admin@mailsnag.com>
2023-06-24 13:16:52 -0500

    * add vuetify

---
+++ app/frontend/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app id="vuetify-app">
+    <v-main>
+      <p> Vuetify 3 App Running 🍾🎉</p>
+      <slot></slot>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
+import { defineConfig } from "vite";
+import RailsPlugin from "vite-plugin-rails";
+import vue from "@vitejs/plugin-vue";
+import vuetify from "vite-plugin-vuetify";
 
 export default defineConfig({
-  plugins: [
-    RubyPlugin(),
-  ],
-})
+  plugins: [RailsPlugin({ stimulus: false }), vue(), vuetify()],
+  resolve: {
+    alias: {
+      vue: "vue/dist/vue.esm-bundler.js",
+    },
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue", ".sass", ".scss", ".css", ".png", ".svg"],
+  },
+  appType: "mpa",
+  clearScreen: false,
+});
